486143f6b2f64431374cbe5fb45040d1ca4b9182
Ho Sy Son <emanhxtanh@gmail.com>
2024-09-25 15:39:00 +0700

    hoan thanh giao dien

--- app/src/main/java/com/example/ex01/MainActivity.java
+++ app/src/main/java/com/example/ex01/MainActivity.java
@@ -10,8 +10,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 
 public class MainActivity extends AppCompatActivity {
-    EditText edtA,edtB,edtKQ;
-    Button btnTong;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,19 +17,5 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
-        edtA = findViewById(R.id.edtA);
-        edtB = findViewById(R.id.edtB);
-        edtKQ =findViewById(R.id.edtKQ);
-        btnTong = findViewById(R.id.btnTong);
-
-        btnTong.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                int a = Integer.parseInt(edtA.getText().toString());
-                int b = Integer.parseInt(edtB.getText().toString());
-                int c = a+b;
-                edtKQ.setText(c+"");
-            }
-        });
     }
 }
