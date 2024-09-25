1c2c69a1022640b67429718da772a93e65f8c3e2
Ho Sy Son <emanhxtanh@gmail.com>
2024-09-25 15:59:14 +0700

    Xu li logic

--- app/src/main/java/com/example/ex01/MainActivity.java
+++ app/src/main/java/com/example/ex01/MainActivity.java
@@ -15,8 +15,19 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
-
+    edtA = findViewById(R.id.edtA);
+    edtB = findViewById(R.id.edtB);
+    edtKQ = findViewById(R.id.edtKQ);
+    btnTong = findViewById(R.id.btnTong);
+    btnTong.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            int a = Integer.parseInt(edtA.getText().toString());
+            int b = Integer.parseInt(edtB.getText().toString());
+            int c = a + b;
+            edtKQ.setText(c+"");
+        }
+    });
     }
 }
