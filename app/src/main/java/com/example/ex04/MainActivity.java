4c1ed3aed1820546a4196666fa0bb188bf1a6a97
Ho Sy Son <emanhxtanh@gmail.com>
2024-10-02 22:48:33 +0700

    Hoan thanh app

--- app/src/main/java/com/example/ex04/MainActivity.java
+++ app/src/main/java/com/example/ex04/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.ex04;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,13 +11,38 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import java.text.DecimalFormat;
 
+public class MainActivity extends AppCompatActivity {
+EditText edtdoC,edtdoF;
+Button btncf,btnfc;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        edtdoC = findViewById(R.id.edtdoC);
+        edtdoF = findViewById(R.id.edtdoF);
+        btncf = findViewById(R.id.btncf);
+        btnfc = findViewById(R.id.btnfc);
+        btncf.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DecimalFormat dcf = new DecimalFormat("#.00");
+                String doC= edtdoC.getText()+"";
+                int C= Integer.parseInt(doC);
+                edtdoF.setText(""+dcf.format(C*1.8+32));
+            }
+        });
+        btnfc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DecimalFormat dcf=new DecimalFormat("#.00");
+                String doF= edtdoF.getText()+"";
+                int F= Integer.parseInt(doF);
+                edtdoC.setText(""+dcf.format((F-32)/1.8));
+            }
+        });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
