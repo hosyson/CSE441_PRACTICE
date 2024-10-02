0b2dbab3b5bff7437de4d113f28c6e737205c43e
Ho Sy Son <emanhxtanh@gmail.com>
2024-10-02 15:18:06 +0700

    Khai bao bien cho cac thanh phan giao dien

--- app/src/main/java/com/example/ex03/MainActivity.java
+++ app/src/main/java/com/example/ex03/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.ex03;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +11,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText edt1, edt2, edt3;
+    Button btncong, btntru, btnnhan, btnchia;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
