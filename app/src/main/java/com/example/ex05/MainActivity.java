0603f3a544d9d215e07696afb6f55c6c7d7f1872
Ho Sy Son <emanhxtanh@gmail.com>
2024-10-05 09:41:56 +0700

    Hoan thanh logic 1

--- app/src/main/java/com/example/ex05/MainActivity.java
+++ app/src/main/java/com/example/ex05/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.ex05;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +14,120 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    TextView txtResultOutput;
+    Button btnMove;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        Button btnDoi=(Button) findViewById(R.id.button1);
+        txtResultOutput = findViewById(R.id.textView4);
+        btnMove = findViewById(R.id.btnMove);
+        btnDoi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText edt;
+                edt = findViewById(R.id.editTextText);
+                int nd = Integer.parseInt(edt.getText().toString());
+                String rs = convert(nd);
+                txtResultOutput.setText(rs);
+            }
+        });
+        btnMove.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+            }
+        });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        };
+
+
+    private String convert(int year){
+        String can,chi;
+        switch (year%10) {
+            case 0:
+                can = "Canh";
+                break;
+            case 1:
+                can = "Tan";
+                break;
+            case 2:
+                can = "Nhâm";
+                break;
+            case 3:
+                can = "Quý";
+                break;
+            case 4:
+                can = "Giáp";
+                break;
+            case 5:
+                can = "Ất";
+                break;
+            case 6:
+                can = "Bính";
+                break;
+            case 7:
+                can = "Đinh";
+                break;
+            case 8:
+                can = "Mậu";
+                break;
+            case 9:
+                can = "Kỷ";
+                break;
+            default:
+                can = "";
+                break;
+        };
+        switch(year%12) {
+            case 0:
+                chi = "Thân";
+                break;
+            case 1:
+                chi = "Dậu";
+                break;
+            case 2:
+                chi = "Tuất";
+                break;
+            case 3:
+                chi = "Hợi";
+                break;
+            case 4:
+                chi = "Tý";
+                break;
+            case 5:
+                chi = "Sửu";
+                break;
+            case 6:
+                chi = "Dần";
+                break;
+            case 7:
+                chi = "Mẹo";
+                break;
+            case 8:
+                chi = "Thìn";
+                break;
+            case 9:
+                chi = "Tỵ";
+                break;
+            case 10:
+                chi = "Ngọ";
+                break;
+            case 11:
+                chi = "Mùi";
+                break;
+            default:
+                chi = "Sai đầu vào";
+                break;
+        }
+        return can +" "+ chi;
     }
-}
+    }
