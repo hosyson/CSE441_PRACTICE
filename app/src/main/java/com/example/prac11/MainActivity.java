d01be7ad8133dc074a7a3ddaff6631f7514cf65c
Ho Sy Son <emanhxtanh@gmail.com>
2024-10-20 20:25:00 +0700

    EX11

---
+++ app/src/main/java/com/example/prac11/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.prac11;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText edt1;
+    Button btnOk;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        edt1 = findViewById(R.id.edt1);
+        btnOk = findViewById(R.id.btnOk);
+
+        btnOk.setOnClickListener(v-> {
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://" + edt1.getText().toString()));
+            startActivity(intent);
+        });
+    }
+}
