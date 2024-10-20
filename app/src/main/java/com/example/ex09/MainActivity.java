76aaad3023e3a012e43ebe4327c9b60ce79e6d02
Ho Sy Son <emanhxtanh@gmail.com>
2024-10-20 19:37:24 +0700

    Hoan thanh MainActivity

--- app/src/main/java/com/example/ex09/MainActivity.java
+++ app/src/main/java/com/example/ex09/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.ex09;
 
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+    ImageButton imageBtnPlay, imageBtnStop;
+    Boolean flag = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +27,33 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        imageView = findViewById(R.id.imageView);
+        imageBtnPlay = findViewById(R.id.imageBtnPlay);
+        imageBtnStop = findViewById(R.id.imageBtnStop);
+
+        imageBtnPlay.setImageResource(R.drawable.play_image);
+        imageBtnStop.setImageResource(R.drawable.stop_image);
+        imageView.setImageResource(R.drawable.image);
+
+        imageBtnPlay.setOnClickListener(v -> {
+            Intent intent = new Intent(this, MyService.class);
+            startService(intent);
+            if(flag == true) {
+                imageBtnPlay.setImageResource(R.drawable.stop_image);
+                flag = false;
+            } else {
+                imageBtnPlay.setImageResource(R.drawable.play_image);
+                flag = true;
+            }
+        });
+
+        imageBtnStop.setOnClickListener(v -> {
+            Intent intent = new Intent(this, MyService.class);
+            stopService(intent);
+            imageBtnPlay.setImageResource(R.drawable.play_image);
+            flag = true;
         });
     }
 }
